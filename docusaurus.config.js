0d21a6e4ee30e4294cb33a77ef6fd41dde9ecfba
Olha Fedorova <srcolgaf@gmail.com>
2026-03-11 14:02:32 +0100

    Update configuration file with new URL, base URL, org name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'hhttps://cyber-helga.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs-as-code-learning/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cyber-helga', // Usually your GitHub org/user name.
+  projectName: 'docs-as-code-learning', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
