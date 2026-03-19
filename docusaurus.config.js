aa27e3c7c02df2f1f0ad13656093fbbe98e08c98
Olha Fedorova <srcolgaf@gmail.com>
2026-03-19 12:57:34 +0100

    Add json and bash to the list of supported languages to enable copy code button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -147,7 +147,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
-        additionalLanguages: ['powershell', 'java', 'csharp', 'php'],
+        additionalLanguages: ['powershell', 'java', 'csharp', 'php', 'bash', 'json'],
       },
     }),
 };
